6df1fd25a79d7dc34c306ba0166d2771ac89d262
kaushik327 <kaushik.r.varadharajan@gmail.com>
2023-03-08 00:21:32 -0600

    drawing in black

--- lib/pages/draw_page.dart
+++ lib/pages/draw_page.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:firebasetutorial/widgets/big_button.dart';
 
+const Size canvasSize = Size(400, 400);
+// TODO: change width and height of draw box
+
 class DrawPage extends StatefulWidget {
   const DrawPage({super.key});
 
@@ -9,11 +12,34 @@ class DrawPage extends StatefulWidget {
 }
 
 class _DrawPageState extends State<DrawPage> {
+
+  List<PointWrapper?> points = [];
+
+  void _onPanStart(DragStartDetails details) {
+    setState(() {
+      points.add(PointWrapper(point: details.localPosition));
+    });
+  }
+  void _onPanUpdate(DragUpdateDetails details) {
+    setState(() {
+      points.add(PointWrapper(point: details.localPosition));
+    });
+  }
+  void _onPanEnd(DragEndDetails details) {
+    setState(() {
+      points.add(null);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.deepPurple,
+        title: const Text(
+          "Draw!",
+          style: TextStyle(fontSize: 16),
+        ),
       ),
       body: SafeArea(
         child: Center(
@@ -21,10 +47,25 @@ class _DrawPageState extends State<DrawPage> {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
+                Padding(
+                  padding: const EdgeInsets.all(25),
+                  child: GestureDetector(
+                    onPanStart: _onPanStart,
+                    onPanUpdate: _onPanUpdate,
+                    onPanEnd: _onPanEnd,
+                    child: CustomPaint(
+                      size: canvasSize,
+                      painter: Painter(
+                        points: points
+                      ),
+                    ),
+                  ),
+                ),
+
                 const SizedBox(height: 25),
                 BigButton(text: 'Save and Quit', onTap: () {
                   Navigator.of(context).pop();
-                })
+                }),
               ],
             ),
           ),
@@ -32,4 +73,41 @@ class _DrawPageState extends State<DrawPage> {
       ),
     );
   }
+}
+
+class Painter extends CustomPainter {
+
+  List<PointWrapper?> points;
+  Painter({required this.points});
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    var paint = Paint()
+      ..color = Colors.black
+      ..strokeWidth = 3.0
+      ..strokeCap = StrokeCap.butt;
+    for (int i = 0; i < points.length - 1; i++) {
+      if (pointIsGood(points[i]) && pointIsGood(points[i+1])) {
+        canvas.drawLine(points[i]!.point, points[i+1]!.point, paint);
+      }
+    }
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
+  }
+
+  bool pointIsGood(PointWrapper? pointWrapper) {
+    if (pointWrapper == null) {
+      return false;
+    }
+    // TODO: check if the point is within the canvas
+    return true;
+  }
+}
+
+class PointWrapper {
+  Offset point;
+  PointWrapper({required this.point});
 }
